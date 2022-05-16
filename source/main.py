4cad40c493a7594fc149beb58bd0b291a3a74ac8
robert <robertgoodwin00@outlook.com>
2022-05-15 21:05:07 -0500

    with planes'

--- source/main.py
+++ source/main.py
@@ -4,7 +4,8 @@
 import os
 import pickle
 import pygame
-
+import planes
+import planes.gui
 
 
 from source.tachie import *
@@ -14,7 +15,7 @@ def get_font(font_name, font_size):
     source_dir = os.path.split(os.path.abspath(__file__))[0]
     main_dir = os.path.join(source_dir,"../")
     font_path = os.path.join(main_dir,"fonts",font_name)
-    #font_path = os.path.join("fonts",font_name)
+        #font_path = os.path.join("fonts",font_name)
     font = pygame.font.Font(font_path, font_size)
     return font
 
@@ -30,55 +31,71 @@ save_data_path = "save_data"
 pygame.init()
 
 
-screen = pygame.display.set_mode((900,600))
+screen = planes.Display((900,600))
+#screen = pygame.display.set_mode((900,600))
 
-screen.fill((20, 20, 160))
+#self.image.fill((20, 20, 160))
+
+screen.image.fill((20, 20, 160))
 
 
 
+class Title_Window(planes.Plane):
+    def __init__(self): 
+
+        #print(pygame.font.get_fonts())
+
+        planes.Plane.__init__(self, 'title window', pygame.Rect((0,0),(900,600)))
+
+        self.refresh()
 
 
-
-background_color = (75,75,75)
-screen.fill(background_color)
+    def refresh(self):
+        background_color = (75,75,75)
+        self.image.fill(background_color)
        
           
-text_surface = get_font("Vera.ttf", 18).render("test message", False, (250,10,10))
-x = 200
-screen.blit(text_surface, (x,35))
-  
+        text_surface = get_font("Vera.ttf", 18).render("test message", False, (250,10,10))
+        x = 200
+        self.image.blit(text_surface, (x,35))
 
 
 
 
-   
-tachie = Tachie()
-got_tachie = tachie.get_tachie()
-source_rect = got_tachie.get_rect()
-vertical_offset = 0
 
 
-screen.blit(got_tachie, (250, 50), source_rect)
+        tachie = Tachie()
+        got_tachie = tachie.get_tachie()
+        source_rect = got_tachie.get_rect()
+        vertical_offset = 0
 
-text_surface = get_font("VeraSe.ttf",14).render("Another message", True, (160,200,75))
-screen.blit(text_surface, (250,120))
 
+        self.image.blit(got_tachie, (250, 50), source_rect)
+
+        text_surface = get_font("VeraSe.ttf",14).render("Another message", True, (160,200,75))
+        self.image.blit(text_surface, (250,120))
+        
+        
+
+
+tw = Title_Window()
+screen.sub(tw)
+
+screen.update()
+screen.render()
 pygame.display.flip() # update the whole screen
         
         
 
-        
-       
   
                 
 def main():
-	while 1:
-		events = pygame.event.get()
-		for event in events:
-
-            	    if event.type == pygame.KEYDOWN and event.unicode == 'q':
-                		pygame.quit()
-                		raise SystemExit            
+    while 1:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.unicode == 'q':
+                pygame.quit()
+                raise SystemExit       
         
   
 
