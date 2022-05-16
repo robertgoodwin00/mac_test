e67dd74e53e0867456b1c74527b010d7e7d11959
robert <robertgoodwin00@outlook.com>
2022-05-15 22:23:28 -0500

    sdfhdss

--- source/main.py
+++ source/main.py
@@ -41,21 +41,21 @@ def load_back(name):
 pygame.init()
 
 
-#screen = planes.Display((900,600))
-screen = pygame.display.set_mode((900,600))
+screen = planes.Display((900,600))
+#screen = pygame.display.set_mode((900,600))
 
 #self.image.fill((20, 20, 160))
 
-screen.fill((20, 20, 160))
+#screen.fill((20, 20, 160))
 
-text_surface = get_font("Vera.ttf", 18).render("test message", False, (250,10,10))
+'''text_surface = get_font("Vera.ttf", 18).render("test message", False, (250,10,10))
 x = 200
 screen.blit(text_surface, (x,35))
         
 text_surface = get_font("VeraSe.ttf",14).render("Another message", True, (160,200,75))
-screen.blit(text_surface, (x,120))
+screen.blit(text_surface, (x,120))'''
 
-'''
+
 class Title_Window(planes.Plane):
     def __init__(self): 
 
@@ -83,6 +83,12 @@ class Title_Window(planes.Plane):
         
         text_surface = get_font("VeraSe.ttf",14).render("Another message", True, (160,200,75))
         self.image.blit(text_surface, (x,120))
+        
+        text_surface = get_font("LiberationSans.ttf",18).render("Yet Another message", True, (160,200,125))
+        self.image.blit(text_surface, (x,180))
+        
+        text_surface = get_font("LiberationSerif.ttf",18).render("Some other message", True, (160,100,175))
+        self.image.blit(text_surface, (x,240))
 
 
 
@@ -109,10 +115,10 @@ class Title_Window(planes.Plane):
 
 
 tw = Title_Window()
-screen.sub(tw)#
+screen.sub(tw)
 
 screen.update()
-screen.render()'''
+screen.render()
 pygame.display.flip() # update the whole screen
         
         
